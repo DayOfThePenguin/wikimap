f52cb16db9d769a298c1acb9fdf20f15b9ad39b7
Colin Dablain <colin.dablain@gmail.com>
2017-05-02 20:56:52 -0400

    added jupyter notebooks

--- rethink.py
+++ rethink.py
@@ -1,27 +1,26 @@
 import logging
-
 import os
 import urlparse
 
 import wikipedia
-
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = False
+
 
 @app.route("/", methods=["POST", "GET"])
 def homepage():
     if request.method == "GET":
         return(render_template("index.html"))
     if request.method == "POST":
-        return(render_template(
-                "search_results.html",
-                query = request.form['idea'],
-                results = wikipedia.search(request.form['idea'], results=5)
-            )
-        )
+        return(render_template("map_view.html",
+                               query=request.form['idea'],
+                               results=wikipedia.search(request.form['idea'],
+                                                        results=5)
+                               )
+               )
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='127.0.0.1', port=5001)
